58298a665faeedf6b194e48dff775c30ac7eee6b
Tom Anderson <tom.anderson@timgroup.com>
2014-01-23 13:28:24 +0000

    Cache the results of file existence checks for a second

--- src/main/java/com/timgroup/log/appender/RotationBasedTriggeringPolicy.java
+++ src/main/java/com/timgroup/log/appender/RotationBasedTriggeringPolicy.java
@@ -5,10 +5,39 @@ import java.io.File;
 import ch.qos.logback.core.rolling.TriggeringPolicyBase;
 
 public class RotationBasedTriggeringPolicy<E> extends TriggeringPolicyBase<E> {
+    protected static class Clock {
+        public long currentTimeMillis() {
+            return System.currentTimeMillis();
+        }
+    }
+
+    private final Clock clock;
+    private long lastExisted;
+
+    protected RotationBasedTriggeringPolicy(Clock clock) {
+        this.clock = clock;
+    }
+
+    public RotationBasedTriggeringPolicy() {
+        this(new Clock());
+    }
 
     @Override
     public boolean isTriggeringEvent(File activeFile, E event) {
-        return !activeFile.exists();
+        return !activeFileExists(activeFile);
+    }
+
+    private boolean activeFileExists(File activeFile) {
+        long now = clock.currentTimeMillis();
+        if (now - lastExisted < 1000) {
+            return true;
+        } else {
+            boolean exists = activeFile.exists();
+            if (exists) {
+                lastExisted = now;
+            }
+            return exists;
+        }
     }
 
 }
--- src/test/java/com/timgroup/log/appender/RotationBasedTriggeringPolicyTest.java
+++ src/test/java/com/timgroup/log/appender/RotationBasedTriggeringPolicyTest.java
@@ -4,16 +4,21 @@ import java.io.File;
 
 import org.junit.Test;
 
+import com.timgroup.log.appender.RotationBasedTriggeringPolicy.Clock;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import static com.timgroup.test.FailingImplementation.STRICT;
 
 public class RotationBasedTriggeringPolicyTest {
 
     private final File existingFile = mock(File.class, STRICT);
+    private final Clock clock = mock(Clock.class, STRICT);
 
     @Test
     public void doesNotTriggerIfFileExists() throws Exception {
@@ -25,10 +30,71 @@ public class RotationBasedTriggeringPolicyTest {
     }
 
     @Test
-    public void doesTriggersIfFileDoesNotExist() throws Exception {
+    public void triggersIfFileDoesNotExist() throws Exception {
         doReturn(false).when(existingFile).exists();
 
         boolean triggering = new RotationBasedTriggeringPolicy<Void>().isTriggeringEvent(existingFile, null);
+
+        assertTrue(triggering);
+    }
+
+    @Test
+    public void doesNotCheckForFileExistenceIfHasPositivelyCheckedRecently() throws Exception {
+        RotationBasedTriggeringPolicy<Void> policy = new RotationBasedTriggeringPolicy<Void>(clock);
+
+        doReturn(1000000L).when(clock).currentTimeMillis();
+
+        checkNotTriggeredByExistingFile(policy);
+
+        doReturn(1000999L).when(clock).currentTimeMillis();
+
+        checkNotTriggeredByExistingFile(policy);
+
+        verify(existingFile, times(1)).exists();
+    }
+
+    @Test
+    public void checksForFileExistenceIfHasNotCheckedRecently() throws Exception {
+        RotationBasedTriggeringPolicy<Void> policy = new RotationBasedTriggeringPolicy<Void>(clock);
+
+        doReturn(1000000L).when(clock).currentTimeMillis();
+
+        checkNotTriggeredByExistingFile(policy);
+
+        doReturn(1001000L).when(clock).currentTimeMillis();
+
+        checkNotTriggeredByExistingFile(policy);
+
+        verify(existingFile, times(2)).exists();
+    }
+
+    @Test
+    public void checksForFileExistenceIfHasNegativelyCheckedRecently() throws Exception {
+        RotationBasedTriggeringPolicy<Void> policy = new RotationBasedTriggeringPolicy<Void>(clock);
+
+        doReturn(1000000L).when(clock).currentTimeMillis();
+
+        checkTriggeredByMissingFile(policy);
+
+        doReturn(1000001L).when(clock).currentTimeMillis();
+
+        checkNotTriggeredByExistingFile(policy);
+
+        verify(existingFile, times(2)).exists();
+    }
+
+    private void checkNotTriggeredByExistingFile(RotationBasedTriggeringPolicy<Void> policy) {
+        doReturn(true).when(existingFile).exists();
+
+        boolean triggering = policy.isTriggeringEvent(existingFile, null);
+
+        assertFalse(triggering);
+    }
+
+    private void checkTriggeredByMissingFile(RotationBasedTriggeringPolicy<Void> policy) {
+        doReturn(false).when(existingFile).exists();
+
+        boolean triggering = policy.isTriggeringEvent(existingFile, null);
 
         assertTrue(triggering);
     }
